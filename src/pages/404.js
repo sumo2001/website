8a6a5d4f2a50a280d533db2dfc3318c0b0510856
Sumanth Dodda <51809378+sumo2001@users.noreply.github.com>
2022-03-02 22:23:48 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,7 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div className="errorpage">
-        <img src={logo} alt="howle"  />
-    </div>
+    <h1> Amigos Hackers, Its a simple Site </h1>
   </Layout>
 )
 
