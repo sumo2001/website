6c6ac9042693de182738af944e313c38bfd823ed
Sumanth Dodda <51809378+sumo2001@users.noreply.github.com>
2022-03-02 21:25:10 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
-import logo from './404.gif';
-<img src={logo} alt="loading..." />
+import logo from './404.gif'
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>404: Not Found</h1>
+    <img src={logo} alt="loading..." />
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+)
+
+export default NotFoundPage
+
+
+
+
